35e99d679a085dfb0dd5be4cb690084a810bf8ef
profeandres <andrescantillo@hotmail.com>
2022-02-27 14:39:16 -0500

    correcion redirects netlify

--- src/components/Simulacro.js
+++ src/components/Simulacro.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export const Simulacro = () => {
   return (
-    <div>Simulacro</div>
+    <div>
+      <h1>Simulacro</h1>
+      <p>Bienvenido a tu  primera prueba, para empezar escoge la materia, temas y dificultad que desees para realizar tu simulacro</p>
+
+      <form action="">
+        <label htmlFor="name"></label>
+      </form>
+    </div>
   )
 }
